f89b831f51226debdf7a3a071b26c3acb485f221
root <root@localhost.localdomain>
2019-11-01 10:32:41 +0800

    kvm

--- kvminstall.sh
+++ kvminstall.sh
@@ -6,7 +6,7 @@ D=`date +%Y%m%d`
 USER_N=`whoami`
 HOSTNAME=`hostname`
 LOGDIR=/opt
-SYS=/opt/centos7.6-kvm
+SYS=/var/lib/libvirt/images/centos7.6-kvm
 IPADDR=`grep "IPADDR" /etc/sysconfig/network-scripts/ifcfg-br0|cut -d= -f 2 `
 FREE=1024
 CPU=1
@@ -24,8 +24,8 @@ echo "$EXP" >/opt/expr.log
 NAME=centos7.6-kvm$EXP
 #虚拟机安装
 echo "================KVM虚拟机安装========================================"
-if [ ! -d "$SYS$EXP.qcow2" ]; then
-qemu-img create -f qcow2 "$SYS$EXP".qcow2 10G >>"$LOGDIR"/kvm_${D}.log
+if [ ! -d "$SYS$EXP" ]; then
+qemu-img create -f qcow2 "$SYS$EXP" 6G >>"$LOGDIR"/kvm_${D}.log
 echo -e "\033[40;33m "$USER_N $DATE $HOSTNAME KVM create directory successfully!"\033[0m" >>"$LOGDIR"/kvm_${D}.log
 fi
 echo "$SYS虚拟机安装文件建成功"     
@@ -35,7 +35,7 @@ virt-install \
 --ram "$FREE" \
 --vcpus "$CPU" \
 --cdrom "$CDROM" \
---disk "$SYS$EXP",size=10 \
+--disk "$SYS$EXP",size=6 \
 --network bridge=br0 \
 --network network=default \
 --graphics vnc,listen=0.0.0.0 \
